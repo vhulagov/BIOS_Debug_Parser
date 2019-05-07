c1b3d32466bfe3566d16a14fb454f6cb9f862469
Vladimir Hulagov <lacitis@yandex-team.ru>
2019-05-07 21:35:29 +0300

    [CONSOLE_PARSER/rmt.py] Made structure and informationality for the results: margins per slot, slots per worst results. Added logging. And cleanups.

--- CONSOLE_PARSER/rmt.py
+++ CONSOLE_PARSER/rmt.py
@@ -3,106 +3,116 @@
 from __future__ import print_function
 
 import re
+import sys
 import json
 import yaml
+import logging
 
+import itertools
 from collections import defaultdict
 
-import benchmark.common
-from benchmark.common import log_msg, yank_api
-from benchmark.conf import Conf, parse_list
-from benchmark.test_result import BasicTestResult
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+    stream=sys.stdout
+)
 
+logger = logging.getLogger()
 
-# Exit error codes
-ERR_RAM_PN_MIXED = 102
-ERR_RAM_CNT_INCORRECT = 103
-ERR_RAM_LOW_FREQ = 104
-ERR_RMT_DATA_MISSING = 120
-
-#class RMT(ram_info, rmt_block):
 class RMT:
     """
     Gather Intel Rank Margin Tool, parse and return the output
     """
-    def __init__(self, conf, ram_info, guidelines_file):
+    def __init__(self, conf, ram_info, signal_integrity_result):
         self.margin_params = ['RxDqs-', 'RxDqs+', 'RxV-', 'RxV+', 'TxDq-', 'TxDq+', 'TxV-', 'TxV+', 'Cmd-', 'Cmd+', 'CmdV-', 'CmdV+', 'Ctl-', 'Ctl+']
 
         self.dimm_params = ['DIMM vendor', 'DRAM vendor', 'RCD vendor', 'Organisation', 'Form factor', 'Freq', 'Prod. week', 'PN', 'hex']
 
         self.ram_info = ram_info
+        self.conf = conf
+        self.result = signal_integrity_result
 
         def tree():
             return defaultdict(tree)
 
-        self.rmt_results = {}
+        self.rmt_results = tree()
         self.rmt_worst_case_result = {}
 
-#        self.test_name = 'signal_integrity'
-#        self.config = conf[self.test_name]
-        self.guidelines_file = guidelines_file
-        # import pdb; pdb.set_trace()
-        self.test_configuration = []
-        self.environment = {}
-        self.tags = {}
-        self.test_result = {}
-        self.idle_latencies_fullset = []
-#        self.rmt_data_required = defaultdict(int)
-#        self.rmt_data_required.update({
-#            'socket_0_info' : 1,
-#            'socket_1_info' : 1,
-#            'dimm_info' : 1
-#            })
-        self.rmt_data_completeness = False
+        self.guidelines_all = yaml.load(open(conf['signal_integrity']['guidelines']))
+        self.dimm_labels = yaml.load(open(conf['node_configuration']['dimm_labels']))
 
     def guidelines(self):
         #self.ram_info
-        #log_msg(ram_info['System']['DDR Freq'])
-        guidelines_raw = yaml.load(open(self.guidelines_file))
-        guidelines = guidelines_raw['common'].copy()
-        print(self.ram_info.keys())
-        guidelines.update(guidelines_raw[self.ram_info['System']['DDR Freq']])
+        #print(json.dumps(self.ram_info['System']['DDR Freq'], indent=2))
+        logger.info("DDR frequency: " + self.ram_info['System']['DDR Freq'])
+        json.dumps(self.guidelines_all, indent=4)
+        guidelines = self.guidelines_all['common'].copy()
+        guidelines.update(self.guidelines_all[self.ram_info['System']['DDR Freq']])
         return guidelines
 
-    def qualification(self):
-	rmt_result_decision = False
-	worst_margin = {}
-	guidelines = get_guidelines(guidelines_file)
-	print("RMT guidelines: " + str(guidelines))
-	print("Worst case result: " + str(rmt_worst_case_result))
-	rmt_result_decision = not any(abs(guidelines[x])>abs(rmt_worst_case_result[x]) for x in guidelines.keys())
-	init_value = next(iter(rmt_worst_case_result))
-	worst_margin[init_value] = rmt_worst_case_result[init_value]
-	for param in margin_params:
-	    rmt_diff = abs(rmt_worst_case_result[param])-abs(guidelines[param])
-	    if rmt_diff > 0:
-		#print("POSITIVE")
-		if abs(abs(rmt_worst_case_result[param])-guidelines[param])<worst_margin.itervalues().next():
-		    worst_margin = {}
-		    worst_margin[param] = rmt_diff
-	    else:
-		#print("NEGATIVE(" + str(param) +"):" + str(rmt_worst_case_result[param]) + " " + str(guidelines[param]))
-		if abs(rmt_worst_case_result[param])-guidelines[param]<worst_margin.itervalues().next():
-		    worst_margin = {}
-		    worst_margin[param] = rmt_diff
-	return rmt_result_decision, worst_margin
+    def get_worst_case(self):
+        def tree():
+            return defaultdict(tree)
 
-#        rmt_result_decision = False
-#        worst_margin = {}
-#        guidelines = self.guidelines()
-#        decision = any(abs(guidelines[x])>abs(self.rmt_worst_case_result[x]) for x in guidelines.keys())
-#        if not decision:
-#            init_value = next(iter(self.rmt_worst_case_result))
-#            worst_margin[init_value] = self.rmt_worst_case_result[init_value]
-#            for param in self.margin_params:
-#                if abs(abs(self.rmt_worst_case_result[param])-abs(guidelines[param]))<worst_margin.itervalues().next():
-#                    worst_margin = {}
-#                    worst_margin[param] = abs(self.rmt_worst_case_result[param])-abs(guidelines[param])
-#                    rmt_result_decision = True
-#        return rmt_result_decision, worst_margin
+        worst_margin_dimm = tree()
+        worst_margin = {}
+        mparam_value = {}
+        if self.rmt_results:
+            for mparam in self.margin_params:
+                self.rmt_worst_case_result[mparam] = {}
+                logger.debug("PARAM: " + mparam)
+                for dimm, rank in self.rmt_results.items():
+                    worst_margin_dimm[dimm] = min(rank.values(), key=lambda x: abs(x[mparam]))
+#                self.rmt_worst_case_result[mparam] = min(worst_margin_dimm.values(), key=lambda x: abs(x[mparam]))[mparam]
+                rmt_worst_case_result_min = min(worst_margin_dimm.values(), key=lambda x: abs(x[mparam]))[mparam]
+                self.rmt_worst_case_result[mparam][rmt_worst_case_result_min] = list()
+                for dimm, params in worst_margin_dimm.items():
+                    try:
+                        worst_dimm = params.values().index(rmt_worst_case_result_min)
+                        self.rmt_worst_case_result[mparam][rmt_worst_case_result_min].append(dimm)
+                    except ValueError:
+                        continue
+            logger.debug(json.dumps(self.rmt_worst_case_result, indent=2))
+        return True
+
+    def qualification(self):
+        outcome = False
+        worst_margin = {}
+        guidelines = self.guidelines()
+        # Check if all parameters satisfy the margin thresholds (guidelines)
+        #print(json.dumps(self.rmt_worst_case_result.keys()[0]))
+
+        outcome = not any(abs(guidelines[x])>abs(self.rmt_worst_case_result[x].keys()[0]) for x in guidelines.keys())
+        if not outcome:
+            self.result.set_status(error='Margin is too bad')
+        #self.result.add_data(['guidelines'], guidelines)
+        self.result.config['guidelines'] = guidelines
+        self.result.add_data(['rmt'], self.rmt_results)
+        self.result.add_data(['worst_case'], self.rmt_worst_case_result)
+        logger.info("RMT guidelines: " + str(guidelines))
+        logger.info("Worst case result: " + str(self.rmt_worst_case_result))
+        init_value = next(iter(self.rmt_worst_case_result))
+        # Getting worst parameter from worst case margin
+        worst_margin[init_value] = self.rmt_worst_case_result[init_value]
+        for param in self.margin_params:
+            param_value_abs = abs(self.rmt_worst_case_result[param].keys()[0])
+            rmt_diff = param_value_abs-abs(guidelines[param])
+            if rmt_diff > 0:
+                logger.debug("RMT param(" + str(param) + "): passed " + str(param_value_abs) + " > " + str(guidelines[param]))
+                if abs(param_value_abs-guidelines[param])<worst_margin.itervalues().next():
+                    worst_margin = {}
+                    worst_margin[param] = rmt_diff
+            else:
+                logger.error("RMT result lower than threshold(" + str(param) + ":" + str(guidelines[param]) + \
+                "):" + str(param_value_abs))
+                if param_value_abs-guidelines[param]<worst_margin.itervalues().next():
+                    worst_margin = {}
+                    worst_margin[param] = rmt_diff
+        self.result.add_data(['worst_margin'], worst_margin)
+        return True
 
     def result_completeness(self):
-        print("Check RMT results completeness...")
+        logger.info("Check RMT results completeness...")
         #node_id, chan_id, dimm_id, rank_id = self.rmt_results.keys().split('.')
         return True
         
@@ -118,83 +128,28 @@ class RMT:
         TxV- (TxVLow) Write DQ VREF margin, low side direction
         TxV+ (TxVHigh) Write DQ VREF margin, high side direction
         """
-        print("Processing RMT results...")
-        self.rmt_worst_case_result_node = {}
+        logger.info("Processing RMT results...")
 
+        rmt_rank_margin = {}
         for line in rmt_block:
-            rmt_rank = re.match(r'N[0-1]\.C[0-5]\.D[01]\.R[0-3]', line)
+            rmt_rank_match = re.match(r'N([0-1])\.C([0-5])\.D([01])\.R([0-3])', line)
             split_line = line.split()
-            if not line or not rmt_rank or len(split_line) != 15: 
+            if not line or not rmt_rank_match or len(split_line) != 15: 
                 continue
             if not split_line[1].lstrip('-').isdigit():
                 continue
-            if rmt_rank:
+            if rmt_rank_match:
                 try:
-                  margins_list = map(int,split_line[1:])
+                    margins_list = map(int,split_line[1:])
+                    rmt_dimm = '.'.join(rmt_rank_match.group(1,2,3))
+                    rmt_dimm_label = str(self.dimm_labels[rmt_dimm])
+                    rmt_rank = 'R' + rmt_rank_match.group(4)
+                    #rmt_rank_margin[rmt_rank] = dict(zip(self.margin_params, margins_list))
+                    self.rmt_results[rmt_dimm_label][rmt_rank] = dict(zip(self.margin_params, margins_list))
                 except:
-                  log_msg("The RMT result is rejected. Format violated:", force=True)
-                  print(line)
-                  return -1
-
-                margin_steps = dict(zip(self.margin_params, margins_list))
-                #self.rmt_results[node_id][chan_id][dimm_id][rank_id] = margin_steps
-                self.rmt_results[rmt_rank] = margin_steps
-#        json.dumps(self.rmt_results, indent=2)
-
-        if self.rmt_results:
-            for key in self.margin_params:
-                self.rmt_worst_case_result_node[key] = (min(self.rmt_results.values(), key=lambda x: abs(x[key]))[key])
-
-            if self.rmt_worst_case_result:
-                for key in self.margin_params:
-                    self.rmt_worst_case_result[key] = min(self.rmt_worst_case_result_node[key], self.rmt_worst_case_result[key])
-            else:
-                self.rmt_worst_case_result = self.rmt_worst_case_result_node
-            
-#            log_msg("RMT worst case result for socket " + str(socket_id))
-#            log_msg(json.dumps(self.rmt_worst_case_result, indent=2))
-#            if ram_info:
-#                if rmt_result_completeness:
-                    
-            
-            return self.rmt_results, self.rmt_worst_case_result
-
-    def result_finalyze(self):
-        result = TestResult(conf, test_name)
-        analyze_rmt(args, conf, result)
-        result.component = components
-        result.finish()
-
-        if rmt_data_completeness:
-            rmt_result_decision, worst_margin = qualification(self.rmt_worst_case_result, args.guidelines)
-        else:
-            log_msg('FAIL! Not enough RMT data!', force=True)
-            sys.exit(ERR_RMT_DATA_MISSING)
-
-        result.add_data(['worst_margin'], worst_margin)
-        if rmt_result_decision:
-            log_msg('PASS', force=True)
-        else:
-            test_result.set_status(error='Margin is too bad')
-
-#        model = 'Unknown'
-#        if result.component and not args.disable_sending:
-#            model = result.component[0].get('model')
-#            result.send_component_info(conf['report']['api_url'], result.component, args.startrek)
-#        tags = [model]
-#        if args.tags:
-#            tags.extend(tag.strip() for tag in args.tags.split(','))
-#        TestResult.add_tags(tags)
-#        log_msg(json.dumps(result.get_result_dict(), indent=2), force=True)
-#        filename = '{0}_{1}_{2}.json'.format(model, test_name, result.started_at)
-#        result.save_to_file(filename)
-#        if not args.disable_sending:
-#            result.send_via_api(conf['report']['api_url'])
-#
-#       
-#        TestResult.environment['baseboard'] = baseboard_mfg + " " + baseboard_product
-#        TestResult.environment['inventory'] = baseboard_serial
-#        TestResult.environment['bmc version'] = bmc_version.lstrip('0')
-#        result.add_data(['worst_case'], self.rmt_worst_case_result)
+                    logger.debug("The RMT result is rejected. Format violated:")
+                    logger.debug(line)
+                    return -1
+        return True
 
 # vim: tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab
