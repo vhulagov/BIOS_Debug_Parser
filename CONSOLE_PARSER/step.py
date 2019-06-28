380f12d189f9a1d6cf4fe36773f759bdc842fdec
Vladimir Hulagov <lacitis@yandex-team.ru>
2019-06-28 14:15:37 +0300

    [CONSOLE_PARSER/step.py] Fixed function of processing STEP Resutl Summary. Added get_label_from_slot func. Fixed patterns to get information from FailedPatternBitMask data.

--- CONSOLE_PARSER/step.py
+++ CONSOLE_PARSER/step.py
@@ -38,8 +38,8 @@ class STEP:
 
     def testplan(self):
         testplan = {
-            self.send_results : [ self.qualification ],
-            self.qualification : [ self.result_completeness ]
+#            self.send_results : [ self.qualification ],
+#            self.qualification : [ self.result_completeness ]
         }
         return testplan
 
@@ -49,48 +49,53 @@ class STEP:
         }
         return dbg_block_processing_rules
 
+    def get_label_from_slot(self, slot_id):
+        dimm_id = '.'.join(slot_id)
+        dimm_label = self.dimm_labels[dimm_id]
+        return dimm_label
+
     def process_step(self, dbg_log_block, dbg_block_name, socket_id):
         logger.info('Processing STEP...')
         self.step_result = {
             'test_mode' : None
         }   
         STEP_TEST_MODE_RE = re.compile(r'^Test Mode : (.*).')
-        STEP_FAILED_PATTERN_RE = re.compile(r'\[FailedPatternBitMask (0x[0-9]+)\] N([0-4])\.C([0-6])\.D([0-3])\. FAIL: R([0-1])\.CID([0-9]).BG([0-9])\.BA([0-9])\.ROW:(0x[0-9a-f]*)\.COL:(0x[0-9a-f])\.DQ([0-7][0-9])\.')
-	STEP_DIMM_RESULT_RE = re.compile(r'^N([0-1])\.C([0-5])\.D([01]):  \[S/N: ([12][0-9][0-4][0-9])_([A-Z0-9]+)\] (Pass|Fail|Empty)\(?(.+?)\)?')
-        dimm_labels = yaml.load(open(self.conf['node_configuration']['dimm_labels']), Loader=yaml.BaseLoader)
-        print(json.dumps(self.ram_info, indent=2))
+        #STEP_FAILED_PATTERN_RE = re.compile(r'\[FailedPatternBitMask (0x[0-9]+)\] N([0-4])\.C([0-6])\.D([0-3])\. FAIL: R([0-1])\.CID([0-9])\.BG([0-9])\.BA([0-9])\.ROW:(0x[0-9a-f]+)\.COL:(0x[0-9a-f]+)\.DQ([0-7][0-9])\.')
+        STEP_FAILED_PATTERN_RE = re.compile(r'\[FailedPatternBitMask (0x[0-9A-F]+)\] N([0-4])\.C([0-6])\.D([0-3])\. FAIL: R([0-1])\.CID([0-9])\.BG([0-9])\.BA([0-9])\.ROW:(0x[0-9a-f]+)\.COL:(0x[0-9a-f]+)\.DQ([0-7][0-9])\.(PPR)?:?([a-zA-Z]+)?\(?([A-Z]+)?\)?')
+	STEP_DIMM_RESULT_RE = re.compile(r'^N([0-1])\.C([0-5])\.D([01]):  \[S/N: ([12][0-9][0-4][0-9])_([A-Z0-9]+)\] (Pass|Fail|Empty)\(?([A-Za-z ]+)?\)?')
+        #print(json.dumps(self.ram_info, indent=2))
         for line in dbg_log_block:
-            #print(line)
             #[FailedPatternBitMask 0x2] N1.C5.D0. FAIL: R1.CID0.BG2.BA3.ROW:0x0001a.COL:0x3f8.DQ24.
-            if re.match(STEP_TEST_MODE_RE, line):
-                #step_result['Test Mode'] = re.match(STEP_TEST_MODE_RE, line).group(1).strip(' .')
-                step_result = dict([[x.strip(' .') for x in line.split(':')]])
+            #[FailedPatternBitMask 0x2] N0.C0.D1. FAIL: R1.CID0.BG2.BA3.ROW:0x07f63.COL:0x118.DQ58.PPR:Done(PASS)
             # Process failed patters records
-            if re.match(STEP_FAILED_PATTERN_RE, line):
+            failed_rank_match = re.match(STEP_FAILED_PATTERN_RE, line)
+            if failed_rank_match:
 		print(line)	
-                failed_rank_match = re.match(STEP_FAILED_PATTERN_RE, line).group(0)
-                print(failed_rank_match)
-                try:
-                    rmt_dimm = '.'.join(rmt_rank_match.group(1,2,3))
-                    rmt_dimm_label = str(self.dimm_labels[rmt_dimm])
-                    self.step_results[rmt_dimm_label][rmt_rank] = dict(zip(self.margin_params, margins_list))
-                except:
-                    logger.debug("The STEP result is rejected. Format violated: " + line)
-                    return False
+                dimm_id = self.get_label_from_slot(failed_rank_match.group(2,3,4))
+#                fail_detail = {
+#                    raw = failed_rank_match.group(0),
+
+                if not dimm_id in self.step_result:
+                    self.step_result[dimm_id] = {}
+                self.step_result[dimm_id].update({ 'serial' : step_dimm_result.group(5) })
+                logger.debug("Founded failed pattern: " + str(dimm_id))
             # Process Result Summary
             step_dimm_result = re.match(STEP_DIMM_RESULT_RE, line)
             if step_dimm_result:
-                dimm_id = '.'.join(step_dimm_result.group(1,2,3))
-                #print(step_dimm_result.lastindex)
-                dimm_slot = dimm_labels[dimm_id]
-                logger.info("Found STEP result for " + dimm_slot)
-#                if step_dimm_result.group(5):
-#                    self.step_result[dimm_slot].update({ 'serial' : step_dimm_result.group(5) })
-#                if step_dimm_result.group(6):
-#                    self.step_result[dimm_slot].update({ 'test_status' : step_dimm_result.group(6) })
+                dimm_id = self.get_label_from_slot(step_dimm_result.group(1,2,3))
+                logger.info("Found STEP result for " + str(dimm_id))
+                # TODO rework to groupdict()
+                if not dimm_id in self.step_result:
+                    self.step_result[dimm_id] = {}
+                if step_dimm_result.group(5):
+                    self.step_result[dimm_id].update({ 'serial' : step_dimm_result.group(5) })
+                if step_dimm_result.group(6):
+                    self.step_result[dimm_id].update({ 'test_status' : step_dimm_result.group(6) })
                 if step_dimm_result.group(7):
-#                    self.step_result[dimm_slot].update({ 'ppr_status' : step_dimm_result.group(7) })
-                    self.step_result[dimm_slot] = { 'ppr_status' : step_dimm_result.group(7) }
+                    self.step_result[dimm_id].update({ 'ppr_status' : step_dimm_result.group(7) })
+            if re.match(STEP_TEST_MODE_RE, line):
+                #step_result['Test Mode'] = re.match(STEP_TEST_MODE_RE, line).group(1).strip(' .')
+                step_result = dict([[x.strip(' .') for x in line.split(':')]])
         print(json.dumps(self.step_result, indent=2))
         return True
 
@@ -100,7 +105,7 @@ class STEP:
             return False
         else:
             try:
-                print(json.dumps(self.result.component, indent=2))
+                #print(json.dumps(self.result.component, indent=2))
                 return True
             except:
                 return False
